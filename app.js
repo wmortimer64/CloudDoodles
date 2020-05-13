22f20d3b0fff1c6e1ddc4cad8b83b8301eaafb88
yit <yit@yit.minedaft.ix.tc>
2020-05-13 16:15:37 -0400

    first working versiongit add .git add .git add .git add .git add .git add .git add .git add .git add .git add .

--- app.js
+++ app.js
@@ -4,11 +4,14 @@ var serv = require('http').Server(app);
 app.get('/',function(req, res) {res.sendFile(__dirname+'/client/index.html');});
 app.use('/client',express.static(__dirname+'/client'));
 serv.listen(80);
+var sendall = function(data) {
+	io.sockets.emit('draw',data);
+}
 var io = require('socket.io')(serv,{});
 io.sockets.on('connection',function(socket) {
 	console.log('connection');
 	socket.on('line',function(data) {
-		console.log(data);
-		socket.emit('draw',data);
+		sendall(data);
 	});
 });
+
