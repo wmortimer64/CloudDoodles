a87ea07773f8a77bc707d7366601db86f8bd5b13
MoCreatureGuy <36866793+MoCreatureGuy@users.noreply.github.com>
2020-09-01 17:07:44 -0400

    Fixed a lot of errors made the site look a lot better by adding proper styling and also separated the script tags into separate files.

--- app.js
+++ app.js
@@ -1,17 +1,32 @@
-var express = require('express');
-var app = express();
-var serv = require('http').Server(app);
-app.get('/',function(req, res) {res.sendFile(__dirname+'/client/index.html');});
-app.use('/client',express.static(__dirname+'/client'));
-serv.listen(80);
-var sendall = function(data) {
-	io.sockets.emit('draw',data);
-}
-var io = require('socket.io')(serv,{});
-io.sockets.on('connection',function(socket) {
-	console.log('connection');
-	socket.on('line',function(data) {
-		sendall(data);
-	});
+const express = require('express');
+const app = express();
+const serv = require('http').createServer(app);
+const io = require('socket.io')(serv, {});
+
+/**
+ * Socket IO
+ */
+io.sockets.on('connection', function (socket) {
+    console.log('connection');
+    socket.on('line', function (data) {
+        sendall(data);
+    });
 });
 
+function sendall(data) {
+    io.emit('draw', data);
+}
+
+/**
+ * Setting the static path
+ */
+app.use('/client', express.static(__dirname + '/client'));
+
+/**
+ *  Routes
+ */
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/client/index.html');
+});
+
+serv.listen(80);
